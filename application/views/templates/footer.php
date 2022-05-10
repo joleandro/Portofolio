44b58bca9d649bb41d4ec456bee0f0b586f48146
Josua Siagian <agianjosua@gmail.com>
2022-05-10 11:08:47 +0700

    add all file portofolio

---
+++ application/views/templates/footer.php
@@ -0,0 +1,12 @@
+<!-- footer section start -->
+    <footer>
+        <span>Created By <a href="">Josua Siagian</a> | <span class="far fa-copyright"></span> 2022 All rights reserved.</span>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="<?= base_url('assetss/js') ?>/script.js"></script>
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,14 @@
+<!-- header section start -->
+
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $tittle; ?></title>
+    <link rel="stylesheet" href="<?= base_url('assetss/css'); ?>/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    <link rel="icon" type="text/css" href="<?= base_url('assetss/images') ?>/icon.jpg">
+</head>
